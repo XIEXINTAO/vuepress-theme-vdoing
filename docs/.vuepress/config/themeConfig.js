6498a5238ebb7aa407133975661ef2361601d406
Sunny <962146347@qq.com>
2021-01-15 18:05:32 +0800

    修改

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -10,7 +10,7 @@ module.exports = {
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
-  editLinks: true, // 启用编辑
+  editLinks: false, // 启用编辑
   editLinkText: '编辑',
 
   //*** 以下配置是Vdoing主题改动和新增的配置 ***//
@@ -46,13 +46,13 @@ module.exports = {
 
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: 'xugaoyi', // 必需
-    link: 'https://github.com/xugaoyi', // 可选的
+    name: 'xiexintao', // 必需
+    // link: 'https://github.com/xugaoyi', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
-    avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
-    name: 'Evan Xu',
+    avatar: 'https://blog.xiexintao.top/qiniu_picGo/5489b5482bfdcbd602bcb835d079eb6.jpg',
+    name: 'Sunny Xie',
     slogan: '前端界的小学生',
   },
   social: {
@@ -62,7 +62,7 @@ module.exports = {
       {
         iconClass: 'icon-youjian',
         title: '发邮件',
-        link: 'mailto:894072666@qq.com',
+        link: 'mailto:962146347@qq.com',
       },
       {
         iconClass: 'icon-github',
@@ -76,11 +76,9 @@ module.exports = {
       },
     ],
   },
-  footer: {
-    // 页脚信息
-    createYear: 2019, // 博客创建年份
-    copyrightInfo:
-      'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
-  },
+ footer: { // 页脚信息
+      createYear: 2020, // 博客创建年份
+      copyrightInfo: 'Sunny Xie | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a> | <a href="https://beian.miit.gov.cn/#/Integrated/index">粤ICP备2020102508号-1</a>', // 博客版权信息，支持a标签
+    },
   htmlModules // 插入hmtl(广告)模块
 }
